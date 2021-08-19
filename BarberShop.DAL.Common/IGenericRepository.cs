127426568f3e74ba59ff721c3084c25768b2e3e9
AlSurick <87325390+AlNill@users.noreply.github.com>
2021-08-19 21:47:24 +0300

    Add Generic repository

---
+++ BarberShop.DAL.Common/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarberShop.DAL.Common
+{
+    public interface IGenericRepository<TEntity> where TEntity: class 
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Create(TEntity item);
+        void Update(TEntity item);
+        void Delete(int id);
+    }
+}
