e98fbcf04af8a8b8fed1517fd5b7877b9e882f4e
AlSurick <87325390+AlNill@users.noreply.github.com>
2021-08-22 13:42:03 +0300

    Add UserModel

--- BarberShop.MVC/Models/UserModel.cs
+++ BarberShop.MVC/Models/UserModel.cs
@@ -5,9 +5,10 @@ namespace BarberShop.MVC.Models
     public class UserModel : PersonModel
     {
         [StringLength(30, ErrorMessage = "Nickname must be short than 30 symbols")]
-        public string Nickname { get; set; }
+        public string NickName { get; set; }
         [Required]
         [Phone]
         public string PhoneNumber { get; set; }
+        public string Password { get; set; }
     }
 }
