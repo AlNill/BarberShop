720898662fd1d0b9bcf3f4444dd719c8915f13b5
AlSurick <87325390+AlNill@users.noreply.github.com>
2021-09-12 20:27:26 +0300

    Seed some data. Remove unusable migrations.

--- BarberShop.DAL.EF/Contexts/ApplicationContext.cs
+++ BarberShop.DAL.EF/Contexts/ApplicationContext.cs
@@ -28,11 +28,66 @@ namespace BarberShop.DAL.EF.Contexts
             string adminRoleName = "Admin";
             string userRoleName = "User";
 
-            // добавляем роли
+            // Seed roles
             Role adminRole = new Role { Id = 1, Name = adminRoleName };
             Role userRole = new Role { Id = 2, Name = userRoleName };
 
+            // Seed users
+            User admin = new User
+            {
+                Id = 1,
+                Name = "Admin",
+                Surname = "Admin",
+                FatherName = "Admin",
+                Password = "Admin",
+                NickName = "Admin",
+                PhoneNumber = "+111223333333",
+                RoleId = 1,
+            };
+
+            User user = new User()
+            {
+                Id = 2,
+                Name = "UserName",
+                Surname = "UserSurname",
+                FatherName = "UserFatherName",
+                Password = "User",
+                NickName = "User",
+                PhoneNumber = "+111223333333",
+                RoleId = 2,
+            };
+
+            // Seed barbers
+            Barber barber1 = new Barber()
+            {
+                Id = 1,
+                Name = "Ivan",
+                Surname = "Ivanov",
+                FatherName = "Ivanovich",
+                Information = "Cool information about barber Ivanov must be here.",
+            };
+
+            Barber barber2 = new Barber()
+            {
+                Id = 1,
+                Name = "Petr",
+                Surname = "Petrov",
+                FatherName = "Petrovich",
+                Information = "Cool information about barber Petrov must be here.",
+            };
+
+            Barber barber3 = new Barber()
+            {
+                Id = 1,
+                Name = "Pup",
+                Surname = "Pupkin",
+                FatherName = "Pupkinovich",
+                Information = "Cool information about barber Pupkin must be here.",
+            };
+
             modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
+            modelBuilder.Entity<User>().HasData(new User[] { admin , user });
+            modelBuilder.Entity<Barber>().HasData(new Barber[] {barber1, barber2, barber3 });
             base.OnModelCreating(modelBuilder);
         }
     }
