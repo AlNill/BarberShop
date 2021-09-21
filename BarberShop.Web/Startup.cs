a53b6cc29777586cec2231044c1a5b0f94509df9
AlSurick <87325390+AlNill@users.noreply.github.com>
2021-09-21 20:29:47 +0300

    Shows barbers with angular

---
+++ BarberShop.Web/Startup.cs
@@ -0,0 +1,70 @@
+﻿using BarberShop.BLL.Interfaces;
+using BarberShop.BLL.Services;
+using BarberShop.DAL.Common;
+using BarberShop.DAL.Common.Models;
+using BarberShop.DAL.EF.Contexts;
+using BarberShop.DAL.EF.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BarberShop.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=BarberShopDb;Trusted_Connection=True;";
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(
+                connectionString
+            ));
+
+            // Inject repositories
+            services.AddScoped<IGenericRepository<Barber>, GenericRepository<Barber>>();
+            services.AddScoped<IGenericRepository<Review>, GenericRepository<Review>>();
+            // Inject services
+            services.AddScoped<IBarberService, BarberService>();
+            services.AddScoped<IReviewService, ReviewService>();
+
+            services.AddControllers();
+            // Add swagger
+            services.AddSwaggerGen();
+            services.AddControllers(options => options.EnableEndpointRouting = false);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
+            app.UseCors("CorsPolicy");
+            app.UseMvc();
+        }
+    }
+}
