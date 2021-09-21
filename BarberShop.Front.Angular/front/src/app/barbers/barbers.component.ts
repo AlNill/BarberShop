9834e6bf10f4e2f1e410aa1529ab660ea7bebc0e
Alexey Horongziy <87325390+AlNill@users.noreply.github.com>
2021-09-21 20:22:48 +0300

    Delete barbers.component.ts

--- BarberShop.Front.Angular/front/src/app/barbers/barbers.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-barbers',
-  templateUrl: './barbers.component.html',
-  styleUrls: ['./barbers.component.scss']
-})
-export class BarbersComponent {}
